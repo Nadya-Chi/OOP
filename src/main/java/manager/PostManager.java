9a2613a9c5d58590d0b896178deed83351c2ace9
Nadya Chistyakova <npodtynchenko@list.ru>
2020-10-19 20:13:03 +0300

    manager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,15 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] searchPost() {
+        return null;
+    }
+
+    public void deletePost() {
+        return;
+    }
+}
